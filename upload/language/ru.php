80063b776554ac2bd9f2d9c293b6d67838176bf2
eugene <eugene.lifescale@gmail.com>
2015-09-29 06:10:31 +0300

    added russian language

---
+++ upload/language/ru.php
@@ -0,0 +1,99 @@
+<?php
+
+/**
+ * LICENSE
+ *
+ * This source file is subject to the GNU General Public License, Version 3
+ * It is also available through the world-wide-web at this URL:
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @package    BitsyBay Engine
+ * @copyright  Copyright (c) 2015 The BitsyBay Project (http://bitsybay.com)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU General Public License, Version 3
+ */
+
+$_translation = array(
+
+// Headers
+'BitsyBay - Sell and Buy Digital Creative with BitCoin' => 'BitsyBay - Продажа и покупка цифрового креатива за Bitcoin',
+
+// Welcome module
+'Looking for a Marketplace to buy or sell digital creative in Bitcoin?' => 'Ищите где купить или продать цифровые работы за Bitcoin?',
+'Then here you are:' => 'Тогда вы там, где нужно:',
+'Trade from any country without bank fees and other restrictions' => 'Продавайте и покупайте с любой точки мира без банковских комиссий и других ограничений',
+'Royalty Free and Exclusive licenses' => 'Эксклюзивные и Роялти-фри лицензии',
+'%s Mb free disk space for all new sellers and +%s Mb for every next sale' => '%s Мб бесплатного дискового пространства для всех продавцов и +%s Мб за каждую следующую продажу',
+'0% seller fee up to 2016, 11% later' => '0% комиссия до 2016 года и 11% после',
+'0% seller fee for project contributors forever' => '0% комиссия для всех контрибуторов на постоянной основе',
+
+// Others modules
+'Last offers' => 'Новые товары',
+'Products not found. Try again later' => 'Товары не найдены. Попробуйте чуть позже.',
+
+// Common
+'Make coins +' => 'Продавать +',
+'Collection' => 'Коллекция',
+'Purchased' => 'Покупки',
+'Favorites' => 'Фавориты',
+'Already have an account?' => 'Уже есть аккаунт?',
+'Email or username' => 'Логин или e-mail',
+'Password' => 'Пароль',
+'Sign Up' => 'Регистрация',
+'Join Us' => 'Регистрация',
+'Join' => 'Присоединиться',
+'Sign In' => 'Войти',
+'or' => 'или',
+'Cancel' => 'Отмена',
+'Submit' => 'Отправить',
+'Search' => 'Поиск',
+'Do not forget' => 'Также не забудьте',
+'Visit the F.A.Q. page' => 'Посетить раздел F.A.Q.',
+'Use search to find specific content' => 'Использовать поиск по сайту',
+'View Terms of Service and License Policy' => 'Ознакомиться с Условиями использования и Политикой лицензирования',
+'e.g.' => 'Например,',
+'by' => 'от',
+'for' => 'для',
+
+// Plural
+'offer' => 'предложение',
+'offers' => 'предложений',
+
+'seller' => 'продавец',
+'sellers' => 'продавцов',
+
+'buyer' => 'покупатель',
+'buyers' => 'покупателей',
+
+// Footer
+'Explore' => 'Проект',
+'About Us' => 'О нас',
+'Our Team' => 'Команда',
+'Terms of Service' => 'Регламент',
+'Licensing Policy' => 'Лицензирование',
+'Affiliate Program' => 'Партнерам',
+'Help Center' => 'Поддержка',
+'F.A.Q' => 'F.A.Q',
+'Abuse Report' => 'Пожаловаться',
+'Please, explain the reason for your report and click on the Submit button' => 'Пожалуйста, опишите причину жалобы и нажмите кнопку «Отправить»',
+'Contact Us' => 'Контакты',
+'Network' => 'Мы в сети',
+'Press about us' => 'Пресса',
+'Language' => 'Язык интерфейса',
+'The %s Project &copy; %s Beta' => 'Проект %s &copy; %s Beta',
+'Search in catalog' => 'Поиск по каталогу',
+
+// Home page
+'Home' => 'Главная',
+
+// Contact page
+'Send your message' => 'Обратная связь',
+'Subject' => 'Тема',
+'Message' => 'Сообщение',
+'Physical address' => 'Фактический адрес',
+'BitsyBay do not have a physical address at present as its only a software resource started by a few enthusiasts. We can however be contacted by means of email through the above form.' => 'BitsyBay не имеет физического адреса',
+
+
+// FAQ Page
+'General F.A.Q.' => 'Раздел F.A.Q.',
+
+);
