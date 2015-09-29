629772561eb53cf19e03c752cb9588aab815c98b
eugene <eugene.lifescale@gmail.com>
2015-09-29 08:33:30 +0300

    added new russian strings

--- upload/language/ru.php
+++ upload/language/ru.php
@@ -15,16 +15,16 @@
 $_translation = array(
 
 // Headers
-'BitsyBay - Sell and Buy Digital Creative with BitCoin' => 'BitsyBay - Продажа и покупка цифрового креатива за Bitcoin',
+'BitsyBay - Sell and Buy Digital Creative with BitCoin' => 'BitsyBay - Магазин авторских шаблонов, фотографий, музыки и видео за Bitcoin',
 
 // Welcome module
-'Looking for a Marketplace to buy or sell digital creative in Bitcoin?' => 'Ищите где купить или продать цифровые работы за Bitcoin?',
-'Then here you are:' => 'Тогда вы там, где нужно:',
-'Trade from any country without bank fees and other restrictions' => 'Продавайте и покупайте с любой точки мира без банковских комиссий и других ограничений',
-'Royalty Free and Exclusive licenses' => 'Эксклюзивные и Роялти-фри лицензии',
-'%s Mb free disk space for all new sellers and +%s Mb for every next sale' => '%s Мб бесплатного дискового пространства для всех продавцов и +%s Мб за каждую следующую продажу',
-'0% seller fee up to 2016, 11% later' => '0% комиссия до 2016 года и 11% после',
-'0% seller fee for project contributors forever' => '0% комиссия для всех контрибуторов на постоянной основе',
+'Looking for a Marketplace to buy or sell digital creative in Bitcoin?' => 'Ищите магазин авторских шаблонов, фотографий, музыки и видео за Bitcoin?',
+'Then here you are:' => 'Тогда вы именно там, где нужно:',
+'Trade from any country without bank fees and other restrictions' => 'Покупайте и продавайте цифровые товары по всему миру без ограничений',
+'Royalty Free and Exclusive licenses' => 'Поддержка эксклюзивных и роялти-фри лицензий',
+'%s Mb free disk space for all new sellers and +%s Mb for every next sale' => 'Бесплатно %s Мб дискового пространства +%s Мб за каждую сделку',
+'0% seller fee up to 2016, 11% later' => 'Комиссия 0% до 2016 года и 11% после',
+'0% seller fee for project contributors forever' => 'Комиссия 0% для всех контрибуторов!',
 
 // Others modules
 'Last offers' => 'Новые товары',
@@ -39,7 +39,7 @@ $_translation = array(
 'Email or username' => 'Логин или e-mail',
 'Password' => 'Пароль',
 'Sign Up' => 'Регистрация',
-'Join Us' => 'Регистрация',
+'Join Us' => 'Присоединиться',
 'Join' => 'Присоединиться',
 'Sign In' => 'Войти',
 'or' => 'или',
@@ -53,16 +53,26 @@ $_translation = array(
 'e.g.' => 'Например,',
 'by' => 'от',
 'for' => 'для',
+'Continue' => 'Продолжить',
+'Send Request' => 'Отправить запрос',
+'Please check the form carefully for errors!' => 'Пожалуйста, тщательно проверьте форму на наличие ошибок!',
+
+// Payments
+'Use wallet' => 'Использовать кошелек',
+'Send exactly %s to this address:' => 'Оплатите точно %s по этому адресу:',
 
 // Plural
 'offer' => 'предложение',
-'offers' => 'предложений',
+'offers' => 'предложения',
+'offers ' => 'предложений',
 
 'seller' => 'продавец',
-'sellers' => 'продавцов',
+'sellers' => 'продавца',
+'sellers ' => 'продавцов',
 
 'buyer' => 'покупатель',
-'buyers' => 'покупателей',
+'buyers' => 'покупателя',
+'buyers ' => 'покупателей',
 
 // Footer
 'Explore' => 'Проект',
@@ -92,8 +102,69 @@ $_translation = array(
 'Physical address' => 'Фактический адрес',
 'BitsyBay do not have a physical address at present as its only a software resource started by a few enthusiasts. We can however be contacted by means of email through the above form.' => 'BitsyBay не имеет физического адреса',
 
+// Account
+'Account' => 'Учетная запись',
+'Who are you?' => 'Кто вы?',
+'Password is required' => 'Необходимо указать пароль!',
+'Username is required' => 'Необходимо логин или e-mail!',
+'Username is not exists' => 'Пользователя с таким логином не существует!',
+'E-Mail is not exists' => 'Пользователя с таким e-mail не существует!',
+'Password must be between %s and %s characters' => 'Пароль не может быть короче %s символов или больше %s!',
+'Oops! Incorrect login data.' => 'Неверно указаны логин или пароль!',
+'You can' => 'Вы можете',
+'reset your password here' => 'сбросить ваш пароль здесь',
+'Signed in as' => 'Вы вошли как',
+'Profile' => 'Профиль',
+'Subscriptions' => 'Подписки',
+'Account settings' => 'Настройки',
+'Affiliate' => 'Партнерская программа',
+'Logout' => 'Выход',
+
+
+// Forgot Password
+'Forgot Password' => 'Напомнить пароль',
+
+// Account recovery
+'Account recovery' => 'Восстановление доступа',
+'Recovery' => 'Восстановление',
+'What’s your registered email?' => 'Введите e-mail, указанный при регистрации',
+'I’ve remember my password!' => 'Я вспомнил мой пароль!',
+'Help resources' => 'Нужна помощь?',
+
 
 // FAQ Page
 'General F.A.Q.' => 'Раздел F.A.Q.',
+
+// Notifications
+'Notifications' => 'Оповещения',
+'Filter' => 'Фильтр',
+'Unread' => 'Не прочтенные',
+'All notifications' => 'Все оповещения',
+'No new notifications.' => 'Нет новых оповещений.',
+
+// Products
+'Products' => 'Товары',
+'favorites' => 'избранные',
+'purchased' => 'приобретенные',
+
+// Verification
+'Verification' => 'Верификация',
+'Account Verification' => 'Верификация учетной записи',
+'Proof information is required!' => 'Без бумажки — мы букашки :)',
+'I agree that my request may be decline.' => 'Я соглашаюсь с тем, что мой запрос может быть отклонен.',
+'I agree that my payment no refund.' => 'Я соглашаюсь с тем, что средства не возвращаются.',
+'Step 1: Verification Fee' => 'Шаг 1: Оплата верификации',
+'Step 2: Proof Arguments' => 'Шаг 2: Доказательная база',
+'Step 3: Confirmation' => 'Шаг 3: Подтверждение',
+'The verification process is based on our expert judgement and we charge a small fee for this.' => 'Процесс верификации основан на экспертной оценке, поэтому мы просим за эту услугу символическую плату.',
+'Be sure that the information provided is correct and that all relevant facts and links are provided.' => 'Убедитесь, что предоставляемая информация достоверна и подкреплена реальными фактами.',
+'We do not refund any payments and neither do we guarantee that your request will be approved.' => 'Мы не возвращаем средства и не гарантируем верификацию вашей учетной записи.',
+'The review and verification process takes between 1 to 14 days.' => 'Время рассмотрения заявки составляет от 1 до 14 дней.',
+'The Verification feature is intended primarily for sellers. Verified seller can be identified by this label' => 'Функция верификации предназначена в первую очередь для продавцов, хотя в будущем будет полезна и для покупателей. Верифицированный продавец отмечается знаком ',
+'Verified Seller' => 'Верифицированный продавец',
+'in the product pages.' => 'на страницах товаров.',
+'Verified sellers enjoy a greater deal of trust from buyers.' => 'Продавцы с данным знаком пользуются большим доверием среди покупателей, что позитивно отражается на продажах.',
+'Provide detailed information that in your opinion proves and verifies your identity. It should include at least one web page or social profile that presents your works:' => 'Предоставьте информацию, которая способна подтвердить подлинность вашего аккаунта. Также необходимо указать по крайней мере один веб-сайт или страницу социальной сети, где представлены ваши работы.',
+'Copy the top code and post it on your social profile page or include the bottom code on your site\'s homepage. This is to verify that you are the owner/administrator of the relevant site and or page.' => 'Скопируйте и вставьте один из кодов проверки на страницу вашего веб-сайта или социального профиля. Мы должны убедиться, что указанный ресурс принадлежит именно вам.',
 
 );
