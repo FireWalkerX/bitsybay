d9535d3da2165c3c2136264858990812a5bceb80
eugene <eugene.lifescale@gmail.com>
2015-09-30 19:31:37 +0300

    added video and audio server data

---
+++ database/data/audio_server.sql
@@ -0,0 +1,1 @@
+INSERT INTO `audio_server` VALUES (1,'SoundCloud','https://soundcloud.com','https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/');
---
+++ database/data/video_server.sql
@@ -0,0 +1,3 @@
+INSERT INTO `video_server` VALUES (1,'YouTube','https://www.youtube.com/','https://www.youtube.com/embed/');
+INSERT INTO `video_server` VALUES (2,'Vimeo','https://vimeo.com/','https://player.vimeo.com/video/');
+
