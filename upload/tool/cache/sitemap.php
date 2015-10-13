6674f7339d50da090e3844de1545e8ba38e3b7f0
eugene <eugene.lifescale@gmail.com>
2015-10-14 01:44:24 +0300

    multilingual sitemap support

--- upload/tool/cache/sitemap.php
+++ upload/tool/cache/sitemap.php
@@ -45,6 +45,11 @@ try {
     exit;
 }
 
+// Init helpers
+function canonical($language_id, $language_code, $url) {
+    return $language_id == DEFAULT_LANGUAGE_ID ? $url : str_replace(URL_BASE, URL_BASE . $language_code . '/', $url);
+}
+
 // Init request
 $request = new Request();
 
@@ -60,16 +65,22 @@ $url = new Url($db, $request, $response, URL_BASE);
 $sitemap = new Sitemap(URL_BASE);
 $sitemap->setPath(DIR_BASE . DIR_SEPARATOR . 'public' . DIR_SEPARATOR);
 
+// Get alt languages
+$statement = $db->query('SELECT * FROM `language`');
+$languages = array(); foreach ($statement->fetchAll() as $language) $languages[$language->language_id] = $language->code;
 
 // Add information Pages
-$sitemap->addItem($url->link('common/information/about'), '1.0', 'monthly');
-$sitemap->addItem($url->link('common/information/team'), '1.0', 'monthly');
-$sitemap->addItem($url->link('common/information/terms'), '1.0', 'monthly');
-$sitemap->addItem($url->link('common/information/licenses'), '1.0', 'monthly');
-$sitemap->addItem($url->link('common/information/faq'), '1.0', 'monthly');
-$sitemap->addItem($url->link('common/contact'), '1.0', 'yearly');
-$sitemap->addItem($url->link('account/account/login'), '1.0', 'yearly');
-$sitemap->addItem($url->link('account/account/create'), '1.0', 'yearly');
+foreach ($languages as $language_id => $language_code) {
+    $sitemap->addItem(canonical($language_id, $language_code, $url->link('common/information/about')), '1.0', 'monthly');
+    $sitemap->addItem(canonical($language_id, $language_code, $url->link('common/information/team')), '1.0', 'monthly');
+    $sitemap->addItem(canonical($language_id, $language_code, $url->link('common/information/terms')), '1.0', 'monthly');
+    $sitemap->addItem(canonical($language_id, $language_code, $url->link('common/information/licenses')), '1.0', 'monthly');
+    $sitemap->addItem(canonical($language_id, $language_code, $url->link('common/information/faq')), '1.0', 'monthly');
+    $sitemap->addItem(canonical($language_id, $language_code, $url->link('common/contact')), '1.0', 'yearly');
+    $sitemap->addItem(canonical($language_id, $language_code, $url->link('account/account/login')), '1.0', 'yearly');
+    $sitemap->addItem(canonical($language_id, $language_code, $url->link('account/account/create')), '1.0', 'yearly');
+}
+
 
 // Generate categories
 $statement = $db->query('SELECT `c`.`category_id`,
@@ -79,7 +90,9 @@ $statement = $db->query('SELECT `c`.`category_id`,
 
 if ($statement->rowCount()) {
     foreach ($statement->fetchAll() as $category) {
-        $sitemap->addItem($url->link('catalog/category', 'category_id=' . $category->category_id), '0.9', 'daily', $category->date_modified);
+        foreach ($languages as $language_id => $language_code) {
+            $sitemap->addItem(canonical($language_id, $language_code, $url->link('catalog/category', 'category_id=' . $category->category_id)), '0.9', 'daily', $category->date_modified);
+        }
     }
 }
 
@@ -88,28 +101,29 @@ $statement = $db->query('SELECT `product_id`, `date_modified` FROM `product` WHE
 
 if ($statement->rowCount()) {
     foreach ($statement->fetchAll() as $product) {
-        $sitemap->addItem($url->link('catalog/product', 'product_id=' . $product->product_id), '0.8', 'weekly', $product->date_modified);
+        foreach ($languages as $language_id => $language_code) {
+            $sitemap->addItem(canonical($language_id, $language_code, $url->link('catalog/product', 'product_id=' . $product->product_id)), '0.8', 'weekly', $product->date_modified);
+        }
     }
 }
 
-// Generate search links
-$statement = $db->prepare('SELECT `td`.`name`,
-                                (SELECT MAX(`p`.`date_modified`)
-                                    FROM `product` AS `p`
-                                    JOIN `product_to_tag` AS `p2t` ON (`p2t`.`product_id` = `p`.`product_id`)
-                                    WHERE `p`.`status` = 1 AND `p2t`.`tag_id` = `td`.`tag_id`) AS `date_modified`
-                                FROM `tag_description` AS `td`
-                                WHERE `td`.`language_id` = ?
-                                HAVING `date_modified` IS NOT NULL');
+// Generate tag links
 
-$statement->execute(array(DEFAULT_LANGUAGE_ID));
+$statement = $db->query('SELECT `td`.`name`,
+                            (SELECT MAX(`p`.`date_modified`)
+                                FROM `product` AS `p`
+                                JOIN `product_to_tag` AS `p2t` ON (`p2t`.`product_id` = `p`.`product_id`)
+                                WHERE `p`.`status` = 1 AND `p2t`.`tag_id` = `td`.`tag_id`) AS `date_modified`
+                            FROM `tag_description` AS `td`
+                            HAVING `date_modified` IS NOT NULL');
 
 if ($statement->rowCount()) {
     foreach ($statement->fetchAll() as $search) {
-        $sitemap->addItem($url->link('catalog/search', 'q=' . urlencode($search->name)), '0.7', 'weekly', $search->date_modified);
+        foreach ($languages as $language_id => $language_code) {
+            $sitemap->addItem(canonical($language_id, $language_code, $url->link('catalog/search', 'q=' . rawurlencode($search->name))), '0.7', 'weekly', $search->date_modified);
+        }
     }
 }
-
 
 // Save sitemap
 $sitemap->createSitemapIndex(URL_BASE);
