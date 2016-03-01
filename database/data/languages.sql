87b6ecc370e9e1f7cae097fb55315e48665e0363
eugene <eugene.lifescale@gmail.com>
2016-03-01 20:47:15 +0200

    add Ukrainian language

--- database/data/languages.sql
+++ database/data/languages.sql
@@ -1,3 +1,4 @@
 INSERT INTO `language` (`code`, `locale`, `name`) VALUES ('en','en_UK','English');
 INSERT INTO `language` (`code`, `locale`, `name`) VALUES ('ru','ru_RU','Русский');
+INSERT INTO `language` (`code`, `locale`, `name`) VALUES ('uk','uk_UA','Українська');
 
--- database/data/order_statuses.sql
+++ database/data/order_statuses.sql
@@ -1,3 +1,4 @@
 INSERT INTO `order_status` (`order_status_id`) VALUES (1),(2),(3);
 INSERT INTO `order_status_description` (`order_status_id`, `language_id`, `name`) VALUES (1,1,'Pending'),(2,1,'Approved'),(3,1,'Processed');
-INSERT INTO `order_status_description` (`order_status_id`, `language_id`, `name`) VALUES (1,2,'В ожидании'),(2,1,'Подтверждено'),(3,1,'В обработке');
+INSERT INTO `order_status_description` (`order_status_id`, `language_id`, `name`) VALUES (1,2,'В ожидании'),(2,2,'Подтверждено'),(3,2,'В обработке');
+INSERT INTO `order_status_description` (`order_status_id`, `language_id`, `name`) VALUES (1,3,'В очікуванні'),(2,3,'Підтверджено'),(3,3,'В обробці');
